6c499734af2fbfcd18aee925a4c3ef2d4443877c
Marcano <luissilvam537@gmail.com>
2026-09-23 09:31:00 -0300

    feat(web): registra os serviços de dominio no container de DI

---
+++ masterdatalab.domain/Extensions/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using masterdatalab.domain.Services;
+
+namespace masterdatalab.domain.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            services.AddScoped<PedidoService>();
+            services.AddScoped<PedidoItemService>();
+            services.AddScoped<CadastroService>();
+            return services;
+        }
+    }
+}
