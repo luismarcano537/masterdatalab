abc48907bf52c600fdb0a19b6583b0cadb20f512
Marcano <luissilvam537@gmail.com>
2026-09-23 09:39:51 -0300

    feat(web): adiciona tela generica de cadastros por elemento

---
+++ masterdatalab.web/Controllers/CadastroController.cs
@@ -0,0 +1,25 @@
+﻿using JJMasterData.Web.Extensions;
+using masterdatalab.domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace masterdatalab.web.Controllers
+{
+    public class CadastrosController(CadastroService service) : Controller
+    {
+        [Route("Cadastros/{elementName}")]
+        public async Task<IActionResult> Index(string elementName)
+        {
+            if (!CadastroService.Elementos.ContainsKey(elementName))
+                return NotFound();
+
+            var result = await service.SetupFormViewAsync(elementName);
+
+            if (result is IActionResult actionResult)
+                return actionResult;
+
+            ViewBag.Content = result.HtmlContent;
+            ViewBag.Titulo = CadastroService.Elementos[elementName];
+            return View();
+        }
+    }
+}
